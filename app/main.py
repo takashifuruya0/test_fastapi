78fdac69098b6c047aa389fed856446954117865
Takashi Furuya <takashi.furuya.0@gmail.com>
2023-03-26 22:34:00 +0900

    Completed to QueryParameters

--- app/main.py
+++ app/main.py
@@ -1,27 +1,11 @@
-from enum import Enum
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Annotated
+from fastapi import FastAPI, Query
+from app.models import (
+    Item, DrinkType, DrinkMaster, DrinkType,
+    fake_drink_db, fake_maker_db)
+
 
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = False
-
-class DrinkType(str, Enum):
-    BEER = "beer"
-    WINE = "wine"
-    SAKE = "sake"
-
-fake_drink_db = [
-    {"name": "TAHOE DEEP", "type": DrinkType.BEER},
-    {"name": "Faction Pale", "type": DrinkType.BEER},
-    {"name": "Chardonnay", "type": DrinkType.WINE},
-    {"name": "Cabernet Sauvignon", "type": DrinkType.WINE},
-    {"name": "一ノ蔵 辛口", "type": DrinkType.SAKE},
-]
 
 
 @app.get("/")
@@ -30,13 +14,24 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
+def read_item(
+    item_id: int, 
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string!",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None
+):
     return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
 
 @app.get("/drink")
 async def get_drinks(skip: int = 0, limit: int = 10):
@@ -45,7 +40,7 @@ async def get_drinks(skip: int = 0, limit: int = 10):
 
 
 @app.get("/drink/{drink_id}")
-async def get_drink(drink_id: int, q2:str, q: Union[str, None] = None):
+async def get_drink(drink_id: int, q2:str, q: str|None = None):
     return {"drink": fake_drink_db[drink_id], "q": q, "q2": q2}
 
 
---
+++ app/models.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = False
+
+
+class DrinkType(str, Enum):
+    BEER = "beer"
+    WINE = "wine"
+    SAKE = "sake"
+
+
+class Maker(BaseModel):
+    name: str
+    state: str
+    country: str
+
+
+class DrinkMaster(BaseModel):
+    drink_type: DrinkType
+    maker: Maker
+    name: str
+    amount: int
+    price: float
+    
+
+fake_maker_db = [
+    Maker(name="Russian River Brewing", state="California", country="United States"),
+    Maker(name="Knee Deep Brewing", state="California", country="United States"),
+]
+
+fake_drink_db = [
+    DrinkMaster(
+        drink_type=DrinkType.BEER, maker=fake_maker_db[1], 
+        name="TAHOE DEEP", amount=473, price=1000),
+    DrinkMaster(
+        drink_type=DrinkType.BEER, maker=fake_maker_db[0], 
+        name="Pliney the Elder", amount=473, price=1200),
+    # {"name": "TAHOE DEEP", "type": DrinkType.BEER},
+    # {"name": "Faction Pale", "type": DrinkType.BEER},
+    # {"name": "Chardonnay", "type": DrinkType.WINE},
+    # {"name": "Cabernet Sauvignon", "type": DrinkType.WINE},
+    # {"name": "一ノ蔵 辛口", "type": DrinkType.SAKE},
+]
