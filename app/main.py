3015ca2fc18bf0fdb309245d31e8ffe51cbac61d
Takashi Furuya <takashi.furuya.0@gmail.com>
2023-03-28 14:38:37 +0900

    Completed to FormData

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 from typing import Annotated
-from fastapi import FastAPI, Query, Path, Body, Header, status
+from fastapi import FastAPI, Query, Path, Body, Header, Form, status
 from app.models import (
     Item, DrinkType, DrinkMaster, DrinkType, Maker,
     fake_drink_db, fake_maker_db,
@@ -93,3 +93,14 @@ def fake_save_user(user_in: UserIn):
 async def create_user(user_in: UserIn)->UserOut:
     user_saved = fake_save_user(user_in)
     return user_saved
+
+
+@app.post("/login/")
+async def login(
+    username: Annotated[str, Form()], 
+    email: Annotated[str, Form()], 
+    password: Annotated[str, Form()]
+) -> UserOut:
+    """Login Form"""
+    hashed_password = fake_password_hasher(password)
+    return {"username": username, "email": email, "hashed_password": hashed_password}
